17b4912e6d9f58831bbcf2c0b35e412d4ebed244
lozukka <lotsulotsu@gmail.com>
2025-09-25 12:49:15 +0300

    mobile nav

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const openBtn = document.getElementById("mobile-nav");
+const closeBtn = document.getElementById("close-nav");
+const mobileNavWrapper = document.getElementById("mobile-nav-wrapper");
+
+openBtn.addEventListener("click", (event) => {
+  //change nav class to open
+  mobileNavWrapper.style.display = "flex";
+  document.querySelector("nav").classList.add("show");
+});
+closeBtn.addEventListener("click", (event) => {
+  mobileNavWrapper.style.display = "none";
+  document.querySelector("nav").classList.remove("show");
+});
+//listen to windows-width -> change to mobile nav
