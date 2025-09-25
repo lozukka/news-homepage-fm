63334d1dad498bc53df837a888c221a50739b76f
lozukka <lotsulotsu@gmail.com>
2025-09-25 14:34:04 +0300

    Update script.js
    removed unused variables

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const openBtn = document.getElementById("mobile-nav");
 const closeBtn = document.getElementById("close-nav");
 const mobileNavWrapper = document.getElementById("mobile-nav-wrapper");
 
-openBtn.addEventListener("click", (event) => {
+openBtn.addEventListener("click", () => {
   mobileNavWrapper.style.display = "flex";
   document.querySelector("nav").classList.add("show");
 });
-closeBtn.addEventListener("click", (event) => {
+closeBtn.addEventListener("click", () => {
   mobileNavWrapper.style.display = "none";
   document.querySelector("nav").classList.remove("show");
 });
